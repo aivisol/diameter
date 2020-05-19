c99970345efede8dccc8a83470fea448d8b5adff
Aivis Olsteins <aivis.olsteins@messagebird.com>
2020-05-19 05:55:30 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 from distutils.core import setup
 setup(
-  name = 'diameter',         # How you named your package folder (MyLib)
-  packages = ['diameter', 'diameter.node'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='BSD',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Diameter protocol implementation for python3',   # Give a short description about your library
-  author = 'aivisol',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  name = 'diameter',
+  packages = ['diameter', 'diameter.node'],
+  version = '0.1',
+  license='BSD',
+  description = 'Diameter protocol implementation for python3',
+  author = 'aivisol',
+  author_email = 'support@datatechlabs.com', 
+  url = 'https://github.com/aivisol/diameter',
+  # download_url = 'https://github.com/aivisol/diameter/v_01.tar.gz',
+  keywords = ['diameter', 'aaa', 'authentication', 'authorization', 'accounting', 'dcca'],
+  install_requires=[
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
   ],
 )
